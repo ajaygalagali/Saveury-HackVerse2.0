d02b99a539cec9680825f2340aaaa69cea71b5fb
ajaygalagali <gvajay321@gmail.com>
2021-02-28 00:56:02 +0530

    static Homepage design

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,6 +2,9 @@ import 'package:expansion_card/expansion_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -77,116 +80,11 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.black,
                   thickness: 1,
                 ),
+
+
+                /*Breakfast*/
                 SizedBox(height: 8,),
-
-                // Breakfast
-                InkWell(
-                  onTap: (){
-                    setState(() {
-                      toggleBreakfast = !toggleBreakfast;
-                    });
-                  },
-                  child: Card(
-                    child: Container(
-                      height: 150,
-                      width: MediaQuery.of(context).size.width,
-
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/breakfast.jpg"),
-                          fit: BoxFit.cover,
-                          alignment: Alignment.center,
-                        ),
-                      ),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          child: Column(
-                            children: [
-                              Text("Breakfast",
-                                  style: GoogleFonts.raleway(
-                                      textStyle: TextStyle(
-                                          fontSize: 32,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white
-                                      )
-                                  )
-                              ),
-
-                              toggleBreakfast ? Text("This is dropdown!",
-                                style: TextStyle(
-                                    fontSize:32,
-                                  color: Colors.white
-                                ),
-                              ) : SizedBox()
-
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8,),
-
-                // Lunch
-                Card(
-                  child: Container(
-                    height: 150,
-                    width: MediaQuery.of(context).size.width,
-
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/lunch2.jpg"),
-                        fit: BoxFit.cover,
-                        alignment: Alignment.center,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("Lunch",
-                          style: GoogleFonts.raleway(
-                              textStyle: TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white
-                              )
-                          )
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8,),
-
-                // Dinner
-                Card(
-                  child: Container(
-                    height: 150,
-                    width: MediaQuery.of(context).size.width,
-
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/dinner2.jpg"),
-                        fit: BoxFit.cover,
-                        alignment: Alignment.center,
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("Dinner",
-                          style: GoogleFonts.raleway(
-                              textStyle: TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white
-                              )
-                          )
-                      ),
-                    ),
-                  ),
-                ),
-
-              ExpansionCard(
+                ExpansionCard(
                 onExpansionChanged: (bool isExpanded){
                   setState(() {
                     toggleBreakfast = !toggleBreakfast ;
@@ -194,14 +92,14 @@ class _HomePageState extends State<HomePage> {
                 },
                 borderRadius: 8,
                 background: Container(
-                  height: 300,
+                  height: 275,
                   width: MediaQuery.of(context).size.width,
 
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage("assets/breakfast.jpg"),
                       fit: BoxFit.cover,
-                      colorFilter: toggleBreakfast ? ColorFilter.srgbToLinearGamma() : ColorFilter.linearToSrgbGamma(),
+                      colorFilter: toggleBreakfast ? ColorFilter.srgbToLinearGamma() : null,
                       alignment: Alignment.center,
                     ),
                   ),
@@ -210,8 +108,8 @@ class _HomePageState extends State<HomePage> {
                   child: Column(
                     children: <Widget>[
                       Align(
-                        alignment: Alignment.center,
-                        child: Text("Dinner",
+                        alignment: Alignment.centerLeft,
+                        child: Text("Breakfast",
                             style: GoogleFonts.raleway(
                                 textStyle: TextStyle(
                                     fontSize: 32,
@@ -226,15 +124,163 @@ class _HomePageState extends State<HomePage> {
                 ),
                 children: <Widget>[
                   Container(
-                    child: Text("Content goes over here ! \n Content goes over here ! \n Content goes over here ! \n Content goes over here ! \n",
-                      style: TextStyle(color: Colors.white),
-                    ),
+                    child: Column(
+                      children: [
+                        DishButton(dishName: "Alo Paratha",),
+                        DishButton(dishName: "Masala Dosa",),
+                        DishButton(dishName: "Idly Vada",),
+
+
+                      ],
+                    )
                   )
                 ],
-              )
+              ),
+
+                /*Lunch*/
+                SizedBox(height: 8,),
+                ExpansionCard(
+                  onExpansionChanged: (bool isExpanded){
+                    setState(() {
+                      toggleLunch = !toggleLunch ;
+                    });
+                  },
+                  borderRadius: 8,
+                  background: Container(
+                    height: 275,
+                    width: MediaQuery.of(context).size.width,
+
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/lunch2.jpg"),
+                        fit: BoxFit.cover,
+                        colorFilter: toggleLunch ? ColorFilter.srgbToLinearGamma() : null,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                  title: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text("Lunch",
+                              style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white
+                                  )
+                              )
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  children: <Widget>[
+                    DishButton(dishName: "Panner Masala",),
+                    DishButton(dishName: "Chapathi Palle",),
+                    DishButton(dishName: "Anna Sambar",),
+                  ],
+                ),
+
+                /*Dinner*/
+                SizedBox(height: 8,),
+                ExpansionCard(
+                  onExpansionChanged: (bool isExpanded){
+                    setState(() {
+                      toggleDinner = !toggleDinner ;
+                    });
+                  },
+                  borderRadius: 8,
+                  background: Container(
+                    height: 275,
+                    width: MediaQuery.of(context).size.width,
+
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/dinner2.jpg"),
+                        fit: BoxFit.cover,
+                        colorFilter: toggleDinner ? ColorFilter.srgbToLinearGamma() : null,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                  title: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text("Dinner",
+                              style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white
+                                  )
+                              )
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  children: <Widget>[
+                    DishButton(dishName: "Chicken Fry",),
+                    DishButton(dishName: "Mutton",),
+                    DishButton(dishName: "Anna Sambar",),
+                  ],
+                ),
+
+
+
+
+
               ],
             ),
 
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DishButton extends StatefulWidget {
+
+  String dishName;
+  DishButton({this.dishName});
+
+  @override
+  _DishButtonState createState() => _DishButtonState();
+}
+
+class _DishButtonState extends State<DishButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: (){
+          print(widget.dishName);
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 1),
+            shape: BoxShape.rectangle,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.dishName,
+              style: GoogleFonts.raleway(
+                textStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18
+                )
+              ),
+            ),
           ),
         ),
       ),
